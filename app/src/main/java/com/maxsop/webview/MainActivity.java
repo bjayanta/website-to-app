faeba9200b6b4d90cb5009645046dd2a703b1ea1
Jayanta Biswas <bjayanta.me@gmail.com>
2020-01-31 13:19:39 +0600

    first commit

---
+++ app/src/main/java/com/maxsop/webview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.maxsop.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        // set view
+        webView = findViewById(R.id.my_webview);
+
+        // configure settings
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // set web view client
+        webView.setWebViewClient(new WebViewClient());
+
+        // set url
+        webView.loadUrl("https://maxsop.com/");
+
+    }
+}
